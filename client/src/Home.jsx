792a3bbdf11c5afbe19d501b79cd145a88fa8b0f
Puggy Potato <qingyaurou@gmail.com>
2025-07-11 15:40:08 +0800

    Somewhat setup the home page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,19 +1,60 @@
+import { useEffect, useState } from "react";
+import {jwtDecode} from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 
 function Home(){
+    const [username,setUsername] = useState("Guest");
+    const [loggedIn,setLoggedIn] = useState(false);
 
-    function checkLoggedIn(){
-        
+    const navigate = useNavigate();
+    useEffect(() =>{
+        const token = localStorage.getItem("token");
+        if(token){
+            try{
+                setLoggedIn(true);
+                const decoded = jwtDecode(token);
+                console.log(decoded);
+                setUsername(decoded.username);
+            }
+            catch(err){
+                console.log("Error:",err);
+            }
+        }
+        else{
+            setUsername("Guest");
+        }
+    },[])
+
+    function logOut(){
+        localStorage.removeItem("token");
+        setLoggedIn(false);
+        navigate("/");
     }
 
+    function appointment(){
+
+    }
+
+    function walkIn(){
+
+    }
 
     return(
         <>
-            <div>
-                <h1>PlaceHolder for Home Page</h1>
+            <div id="container">
+                <div id="navBar">
+                        <button id="home" onClick={() =>navigate("/Home")}>Home</button>
+                        <button id="logOut" onClick={() =>logOut()}>Log Out</button>
+                </div>
+                <div id="homeContainer">
+                    <h1>Welcome {username}</h1>
+                    <button id="appointment" onClick={() =>appointment()}>Make An Appointment</button>
+                    <button id="walkIn" onClick={() =>walkIn()}>Walk In</button>
+                </div>
             </div>
         </>
     )
